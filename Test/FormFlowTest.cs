70e6096126c3f8ccab08bbfa50341c9f9b3f8889
Tomoya Furuichi <tomoya@furuichi-jp.com>
2011-02-20 18:06:47 +0900

    別プロジェクト再追加

---
+++ Test/FormFlowTest.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * メモ
+ * ・ContorolsのIndexは，0から順につけられ，重複するとそれ以上のものを1ずらす．
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using StarlitTwit;
+
+namespace Test
+{
+    public partial class FormFlowTest : Form
+    {
+        public FormFlowTest()
+        {
+            InitializeComponent();
+        }
+
+        int i = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UctlDispTwitRow r = new UctlDispTwitRow(new TwitData());
+            r.BorderStyle = BorderStyle.FixedSingle;
+            r.Name = "Row" + i.ToString();
+            r.SetNameLabel();
+            flowLayoutPanel1.Controls.Add(r);
+            flowLayoutPanel1.Controls.SetChildIndex(r, 1);
+            i++;
+
+            foreach (Control ctl in flowLayoutPanel1.Controls) {
+                Console.WriteLine(ctl.Name + " : " + flowLayoutPanel1.Controls.GetChildIndex(ctl).ToString()); 
+            }
+            Console.WriteLine("----");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (flowLayoutPanel1.Controls.Count > 0) {
+                flowLayoutPanel1.Controls.RemoveAt(0);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctl in flowLayoutPanel1.Controls) {
+                if (!(ctl is UctlDispTwitRow)) { continue; }
+                UctlDispTwitRow r = (UctlDispTwitRow)ctl;
+                //r.SetTweetLabel();
+            }
+        }
+    }
+}
---
+++ Test/TabControlEx.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TwitterClient
+{
+    public class TabControlEx2 : TabControl
+    {
+        private int mouseDownPointX = 0;
+        private int mouseDownPointY = 0;
+        private Rectangle dragBoxFromMouseDown = Rectangle.Empty;
+
+        public TabControlEx2()
+        {
+            AllowDrop = true;
+            ClearDragTarget();
+        }
+
+        protected override void OnDragOver(System.Windows.Forms.DragEventArgs e)
+        {
+            base.OnDragOver(e);
+
+            Point pt = new Point(e.X, e.Y);
+            //We need client coordinates.
+            pt = PointToClient(pt);
+
+            //Get the tab we are hovering over.
+            TabPage hover_tab = GetTabPageByTab(pt);
+
+            //Make sure we are on a tab.
+            if (hover_tab != null) {
+                //Make sure there is a TabPage being dragged.
+                if (e.Data.GetDataPresent(typeof(TabPage))) {
+                    e.Effect = DragDropEffects.Move;
+                    TabPage drag_tab = (TabPage)e.Data.GetData(typeof(TabPage));
+
+                    int item_drag_index = FindIndex(drag_tab);
+                    int drop_location_index = FindIndex(hover_tab);
+
+                    //Don't do anything if we are hovering over ourself.
+                    if (item_drag_index != drop_location_index) {
+                        List<TabPage> pages = new List<TabPage>();
+
+                        //Put all tab pages into an array.
+                        for (int i = 0; i < TabPages.Count; i++) {
+                            //Except the one we are dragging.
+                            if (i != item_drag_index)
+                                pages.Add(TabPages[i]);
+                        }
+
+
+                        //Now put the one we are dragging it at the proper location.
+                        pages.Insert(drop_location_index, drag_tab);
+
+                        //this.Visible = false;
+                        this.SuspendLayout();//★これ大事
+                        //Make them all go away for a nanosec.
+                        TabPages.Clear();
+                        //Add them all back in.
+                        TabPages.AddRange(pages.ToArray());
+                        //Make sure the drag tab is selected.
+                        SelectedTab = drag_tab;
+
+                        this.ResumeLayout();
+                        //this.Visible = true;
+                    }
+                }
+            }
+            else {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+
+            if (dragBoxFromMouseDown != Rectangle.Empty &&
+                !dragBoxFromMouseDown.Contains(e.X, e.Y)) {
+                if (this.TabCount <= 1)
+                    return;
+
+                Point pt = new Point(mouseDownPointX, mouseDownPointY);
+                TabPage tp = GetTabPageByTab(pt);
+
+                if (tp != null) {
+                    DoDragDrop(tp, DragDropEffects.All);
+                }
+            }
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+            ClearDragTarget();
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            ClearDragTarget();
+        }
+
+        private void ClearDragTarget()
+        {
+            dragBoxFromMouseDown = Rectangle.Empty;
+            mouseDownPointX = 0;
+            mouseDownPointY = 0;
+        }
+
+        private void SetupDragTarget(int x, int y)
+        {
+            Size dragSize = SystemInformation.DragSize;
+
+            dragBoxFromMouseDown =
+                new Rectangle(new Point(x - (dragSize.Width / 2),
+                                        y - (dragSize.Height / 2)), dragSize);
+            mouseDownPointX = x;
+            mouseDownPointY = y;
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            ClearDragTarget();
+
+            if (e.Button != MouseButtons.Left ||
+                e.Clicks >= 2)
+                return;
+
+            SetupDragTarget(e.X, e.Y);
+        }
+
+        /// <summary>
+        /// Finds the TabPage whose tab is contains the given point.
+        /// </summary>
+        /// <param name="pt">The point (given in client coordinates) to look for a TabPage.</param>
+        /// <returns>The TabPage whose tab is at the given point (null if there isn't one).</returns>
+        private TabPage GetTabPageByTab(Point pt)
+        {
+            TabPage tp = null;
+
+            for (int i = 0; i < TabPages.Count; i++) {
+                if (GetTabRect(i).Contains(pt)) {
+                    tp = TabPages[i];
+                    break;
+                }
+            }
+
+            return tp;
+        }
+
+        /// <summary>
+        /// Loops over all the TabPages to find the index of the given TabPage.
+        /// </summary>
+        /// <param name="page">The TabPage we want the index for.</param>
+        /// <returns>The index of the given TabPage(-1 if it isn't found.)</returns>
+        private int FindIndex(TabPage page)
+        {
+            for (int i = 0; i < TabPages.Count; i++) {
+                if (TabPages[i] == page)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
