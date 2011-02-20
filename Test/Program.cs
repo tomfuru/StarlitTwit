e54a660afca0f35c64b4b0564b77e3cf678265d1
Tomoya Furuichi <tomoya@furuichi-jp.com>
2011-02-20 18:22:41 +0900

    yield returnde

--- Test/Program.cs
+++ Test/Program.cs
@@ -17,7 +17,33 @@ namespace Test
             Application.SetCompatibleTextRenderingDefault(false);
 
             //Application.Run(new FormFlowTest());
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+
+
+            TestClass c = new TestClass();
+            c.testMain();
         }
+    }
+
+    class TestClass
+    {
+        public void testMain()
+        {
+            IEnumerable<int> enumerable = enum1();
+
+            foreach (var item in enumerable) {
+                Console.Write(item);
+            }
+        }
+
+        private IEnumerable<int> enum1()
+        {
+            for (int i = 0; i < 30; i++)
+			{
+                yield return i;
+			}
+        }
+
+
     }
 }
