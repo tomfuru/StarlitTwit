50c648ed1687587236ddb71da280668b603f8d58
Tomoya Furuichi <tomoya@furuichi-jp.com>
2011-02-22 18:13:37 +0900

    URL短縮はURLShortenerクラスを通じて行うように修正

---
+++ StarlitTwit/Function/URLShortening/IURLShortenInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StarlitTwit
+{
+    /// <summary>
+    /// URL短縮サービスの情報を表すクラスのインタフェースです。
+    /// </summary>
+    public interface IURLShortenInfo
+    {
+        /// <summary>
+        /// URLを短縮します。
+        /// </summary>
+        /// <param name="url">短縮するURL</param>
+        /// <returns>短縮されたURL</returns>
+        string Shorten(string url);
+
+        /// <summary>
+        /// 短縮URLを元に戻します。
+        /// </summary>
+        /// <param name="shortenURL">短縮されたURL</param>
+        /// <returns>短縮するURL</returns>
+        string Expand(string shortenURL);
+
+        /// <summary>
+        /// URLがそのサービスで短縮されたURLかどうかを判別します。
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <returns>そのサービスで短縮されたURLかどうか</returns>
+        bool IsShortenURL(string url);
+    }
+}
--- StarlitTwit/Function/URLShortening/URLShortener.cs
+++ StarlitTwit/Function/URLShortening/URLShortener.cs
@@ -14,13 +14,24 @@ namespace StarlitTwit
     /// </summary>
     public static class URLShortener
     {
-        const string bit_ly_username = @"startf";
-        const string bit_ly_APIKey = @"R_c1e1f4935de9874be813d8178d2adb5a";
-        const string bit_ly_url = @"http://bit.ly/";
-        const string j_mp_url = @"http://j.mp/";
-        
+        private static readonly Dictionary<URLShortenType, IURLShortenInfo> SHORTEN_DIC;
+
         //-------------------------------------------------------------------------------
-        #region +Shorten URL短縮
+        #region 静的コンストラクタ
+        //-------------------------------------------------------------------------------
+        //
+        static URLShortener()
+        {
+            SHORTEN_DIC = new Dictionary<URLShortenType,IURLShortenInfo>()  {
+                {URLShortenType.bit_ly, new ShortenerSet_bit_ly.Shortener_bit_ly()},
+                {URLShortenType.j_mp, new ShortenerSet_bit_ly.Shortener_j_mp()} 
+            };
+        }
+        //-------------------------------------------------------------------------------
+        #endregion (静的コンストラクタ)
+
+        //-------------------------------------------------------------------------------
+        #region +[static]Shorten URL短縮
         //-------------------------------------------------------------------------------
         /// <summary>
         /// URLを短縮します。
@@ -29,38 +40,13 @@ namespace StarlitTwit
         /// <returns>短縮URL</returns>
         public static string Shorten(string url, URLShortenType type)
         {
-            StringBuilder sburl = new StringBuilder();
-            switch (type) {
-                case URLShortenType.bit_ly:
-                case URLShortenType.j_mp: {
-                        if (type == URLShortenType.bit_ly) {
-                            sburl.Append("http://api.bit.ly/");
-                        }
-                        else {
-                            sburl.Append("http://api.j.mp/");
-                        }
-                        sburl.Append(@"v3/shorten?login=");
-                        sburl.Append(bit_ly_username);
-                        sburl.Append("&apiKey=");
-                        sburl.Append(bit_ly_APIKey);
-                        sburl.Append("&longUrl=");
-                        sburl.Append(Utilization.UrlEncode(url));
-                        sburl.Append("&format=xml");
-
-                        string requestUrl = sburl.ToString();
-                        XElement el = GetXmlFromWeb(requestUrl);
-
-                        return el.Element("data").Element("url").Value;
-                    }
-                default:
-                    return null;
-            }
+            return SHORTEN_DIC[type].Shorten(url);
         }
         //-------------------------------------------------------------------------------
         #endregion (Shorten)
 
         //-------------------------------------------------------------------------------
-        #region +Expand 短縮URLを戻す
+        #region +[static]Expand 短縮URLを戻す
         //-------------------------------------------------------------------------------
         /// <summary>
         /// 短縮URLを元に戻します。戻せない場合はそのままのURLが返ります。
@@ -69,22 +55,11 @@ namespace StarlitTwit
         /// <returns>元のURL</returns>
         public static string Expand(string shortenUrl)
         {
-            if (shortenUrl.StartsWith(bit_ly_url) || shortenUrl.StartsWith(j_mp_url)) {
-                StringBuilder sburl = new StringBuilder();
-                sburl.Append(@"http://api.bit.ly/v3/expand?login=");
-                sburl.Append(bit_ly_username);
-                sburl.Append("&apiKey=");
-                sburl.Append(bit_ly_APIKey);
-                sburl.Append("&shortUrl=");
-                sburl.Append(Utilization.UrlEncode(shortenUrl));
-                sburl.Append("&format=xml");
-                
-                string requestUrl = sburl.ToString();
-                XElement el = GetXmlFromWeb(requestUrl);
-
-                return el.Element("data").Element("entry").Element("long_url").Value;
+            foreach (var info in SHORTEN_DIC.Values) {
+                if (info.IsShortenURL(shortenUrl)) {
+                    return info.Expand(shortenUrl);
+                }
             }
-
             return shortenUrl;
         }
         #endregion (+Expand)
@@ -95,7 +70,7 @@ namespace StarlitTwit
         //
         public static bool IsShortenURL(string url)
         {
-            return (url.StartsWith(bit_ly_url) || url.StartsWith(j_mp_url));
+            return SHORTEN_DIC.Values.Any(info => info.IsShortenURL(url));
         }
         #endregion (IsShortenURL)
         //-------------------------------------------------------------------------------
@@ -111,43 +86,6 @@ namespace StarlitTwit
             return !urls.All((url) => IsShortenURL(url));
         }
         #endregion (ExistShortenableURL)
-
-        //-------------------------------------------------------------------------------
-        #region -GetXmlFromWeb Webにリクエストを送信して結果をXmlとして取得します。
-        //-------------------------------------------------------------------------------
-        /// <summary>
-        /// Webにリクエストを送信して結果をXmlとして取得します。
-        /// </summary>
-        /// <param name="url"></param>
-        /// <returns></returns>
-        private static XElement GetXmlFromWeb(string url)
-        {
-            WebRequest req = WebRequest.Create(url);
-            req.Method = "GET";
-            req.ContentType = "application/x-www-form-urlencoded";
-
-            WebResponse res;
-            try {
-                res = req.GetResponse();
-            }
-            catch (WebException ex) {
-                Log.DebugLog(ex);
-                return null;
-            }
-
-            using (Stream resStream = res.GetResponseStream()) {
-                using (StreamReader reader = new StreamReader(resStream, Encoding.ASCII)) {
-                    try {
-                        return XElement.Load(reader);
-                    }
-                    catch (XmlException ex) {
-                        Log.DebugLog(ex);
-                        return null;
-                    }
-                }
-            }
-        }
-        #endregion (GetXmlFromWeb)
     }
 
     //-------------------------------------------------------------------------------
