e1ca319869559fcdaf9d6c04e3bc9e1fa23b1a38
Tomoya Furuichi <tomoya@furuichi-jp.com>
2011-03-06 01:05:49 +0900

    Add

---
+++ UserStreamTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using StarlitTwit;
+
+namespace UserStreamTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SettingsData data = SettingsData.Restore();
+
+            Twitter twitter = new Twitter();
+            
+            
+
+            twitter.userstream_statuses_sample();
+        }
+    }
+}
