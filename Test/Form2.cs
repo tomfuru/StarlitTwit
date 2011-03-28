9e0083491a3ec7d7b6e6e919dbd29a1c8c0a2768
Tomoya Furuichi <tomoya@furuichi-jp.com>
2011-03-28 18:22:45 +0900

    画像アニメーション制御クラス追加

--- Test/Form2.cs
+++ Test/Form2.cs
@@ -7,16 +7,102 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Threading;
+using StarlitTwit;
+
+#pragma warning disable 414 // 未使用変数警告
 
 namespace Test
 {
     public partial class Form2 : Form
     {
+
         public Form2()
         {
             InitializeComponent();
-
-            Thread t1;
         }
+
+        //-------------------------------------------------------------------------------
+        #region ThreadTest1
+        //-------------------------------------------------------------------------------
+        Thread t1;
+        Thread t2;
+        int i1 = 0;
+        int i2 = 0;
+
+        private void f1_1()
+        {
+            this.Invoke(new Action(() =>
+            {
+                Thread.Sleep(2000);
+                i1 = 1;
+            }));
+        }
+        private void f1_2()
+        {
+            Thread.Sleep(1000);
+
+            this.Invoke(new Action(() =>
+            {
+                i2 = 1;
+            }));
+        }
+
+        int i3 = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            t1 = new Thread(f1_1) { IsBackground = true };
+            t2 = new Thread(f1_2) { IsBackground = true };
+
+            t1.Start();
+            t2.Start();
+
+            for (int i = 0; i < 1000000; i++) {
+                i3++;
+            }
+        }
+        //-------------------------------------------------------------------------------
+        #endregion (ThreadTest1)
+
+        ImageAnimation anim;
+
+        private void f2_1()
+        {
+            Image img;
+            using (Image img2 = Image.FromFile(@"H:\My Documents\Visual Studio 2010\Projects\TwitterClient\StarlitTwit\Resources\NowLoadingS.gif")) {
+                img = (Image)img2.Clone();
+            }
+            anim = new ImageAnimation(img);
+            anim.FrameUpdated += new EventHandler(Animation_FrameUpdated);
+            anim.StartAnimation();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (anim == null) { f2_1(); }
+            else if (anim.Animating) { anim.StopAnimation(); }
+            else { anim.StartAnimation(); }
+        }
+
+        public void Animation_FrameUpdated(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (anim != null) {
+                e.Graphics.DrawImage(anim.Image, 0, 0, panel1.ClientSize.Width, panel1.ClientSize.Height);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (anim != null) {
+                anim.ResetAnimation();
+            }
+        }
+
+
     }
 }
---
+++ Test/ImageAnimation.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
+
+namespace Test
+{
+    public class ImageAnimation : IDisposable
+    {
+        //-------------------------------------------------------------------------------
+        #region Variables
+        //-------------------------------------------------------------------------------
+        /// <summary>現在のループカウント</summary>
+        private int _nowLoopCount = 0;
+        /// <summary>現在のフレームカウント</summary>
+        private int _nowFrameCount = 0;
+        /// <summary>アニメーション中か</summary>
+        private bool _animating = false;
+        /// <summary>タイマー</summary>
+        private Timer _timer;
+
+        //-------------------------------------------------------------------------------
+        /// <summary>アニメーションするイメージ</summary>
+        private readonly Image _image;
+        /// <summary>フレームディメンション</summary>
+        private readonly FrameDimension FrameDimension;
+        /// <summary>ループ回数</summary>
+        public readonly int MaxLoopCount;
+        /// <summary>フレーム数</summary>
+        public readonly int MaxFrameCount;
+        /// <summary>フレームのディレイ(10ms単位)</summary>
+        public readonly int[] FrameDelays;
+        /// <summary>無限ループかどうか</summary>
+        public readonly bool LoopInfinity;
+        /// <summary>フレーム操作時にロックするオブジェクト</summary>
+        private readonly object _lockFrame = new object();
+        //-------------------------------------------------------------------------------
+        #endregion (Variables)
+
+        //-------------------------------------------------------------------------------
+        #region Property
+        //-------------------------------------------------------------------------------
+        #region Image プロパティ
+        //-------------------------------------------------------------------------------
+        /// <summary>
+        /// アニメーションするイメージを取得します。
+        /// </summary>
+        public Image Image
+        {
+            get { return _image; }
+        }
+        #endregion (Image)
+        //-------------------------------------------------------------------------------
+        #region NowLoopCount プロパティ
+        //-------------------------------------------------------------------------------
+        /// <summary>
+        /// 現在何回目のループか
+        /// </summary>
+        public int NowLoopCount
+        {
+            get { return _nowLoopCount + 1; }
+        }
+        #endregion (NowLoopCount)
+        //-------------------------------------------------------------------------------
+        #region NowFrameCount プロパティ
+        //-------------------------------------------------------------------------------
+        /// <summary>
+        /// 次表示されるのは何フレーム目か
+        /// </summary>
+        public int NowFrameCount
+        {
+            get { return _nowFrameCount + 1; }
+        }
+        #endregion (NowFrameCount)
+        //-------------------------------------------------------------------------------
+        #region Animating プロパティ
+        //-------------------------------------------------------------------------------
+        /// <summary>
+        /// アニメーション中かどうか
+        /// </summary>
+        public bool Animating
+        {
+            get { return _animating; }
+        }
+        #endregion (Animating)
+        //-------------------------------------------------------------------------------
+        #endregion (Property)
+
+        //-------------------------------------------------------------------------------
+        #region public events
+        //-------------------------------------------------------------------------------
+        public event EventHandler FrameUpdated;
+        //-------------------------------------------------------------------------------
+        #endregion (public events)
+
+        //-------------------------------------------------------------------------------
+        #region Constants
+        //-------------------------------------------------------------------------------
+        const int FRAME_DELAY = 0x5100;
+        const int FRAME_NUM = 0x5101;
+        //-------------------------------------------------------------------------------
+        #endregion (Constants)
+
+        //-------------------------------------------------------------------------------
+        #region Constructor
+        //-------------------------------------------------------------------------------
+        //
+        public ImageAnimation(Image img)
+        {
+            _image = img;
+            FrameDimension = new FrameDimension(img.FrameDimensionsList[0]);
+            MaxFrameCount = img.GetFrameCount(FrameDimension);
+            PropertyItem pItemFrameDelay = img.GetPropertyItem(FRAME_DELAY);
+            PropertyItem pItemFrameNum = img.GetPropertyItem(FRAME_NUM);
+            FrameDelays = new int[MaxFrameCount];
+
+            for (int i = 0; i < MaxFrameCount; i++) {
+                FrameDelays[i] = BitConverter.ToInt32(pItemFrameDelay.Value, 4 * i);
+            }
+            MaxLoopCount = BitConverter.ToInt16(pItemFrameNum.Value, 0);
+
+            LoopInfinity = (MaxLoopCount == 0);
+
+            _timer = new Timer(Timer_Elapsed, null, Timeout.Infinite, Timeout.Infinite);
+            _image.SelectActiveFrame(FrameDimension, 0);
+        }
+        #endregion (Constructor)
+
+        //-------------------------------------------------------------------------------
+        #region Timer_Elapsed タイマーの時間経過時イベント
+        //-------------------------------------------------------------------------------
+        //
+        private void Timer_Elapsed(object o)
+        {
+            NextFrame();
+        }
+        #endregion (Timer_Elapsed)
+
+        //-------------------------------------------------------------------------------
+        #region +StartAnimation アニメーションスタート
+        //-------------------------------------------------------------------------------
+        /// <summary>
+        /// アニメーションをスタートします。
+        /// </summary>
+        public void StartAnimation()
+        {
+            if (_animating) { return; }
+
+            lock (_lockFrame) {
+                _timer.Change(FrameDelays[0] * 10, Timeout.Infinite);
+                _animating = true;
+            }
+        }
+        #endregion (StartAnimation)
+        //-------------------------------------------------------------------------------
+        #region +StopAnimation アニメーションストップ
+        //-------------------------------------------------------------------------------
+        /// <summary>
+        /// アニメーションをストップします。
+        /// </summary>
+        public void StopAnimation()
+        {
+            if (!_animating) { return; }
+
+            lock (_lockFrame) {
+                _timer.Change(Timeout.Infinite, Timeout.Infinite);
+                _animating = false;
+            }
+        }
+        #endregion (StopAnimation)
+        //-------------------------------------------------------------------------------
+        #region +ResetAnimation アニメーションリセット
+        //-------------------------------------------------------------------------------
+        /// <summary>
+        /// リセットして最初からアニメーションを再生します。
+        /// </summary>
+        public void ResetAnimation()
+        {
+            lock (_lockFrame) {
+                if (_animating) { _timer.Change(FrameDelays[0] * 10, Timeout.Infinite); }
+                _image.SelectActiveFrame(FrameDimension, 0);
+                _nowFrameCount = _nowLoopCount = 0;
+
+                if (FrameUpdated != null) { FrameUpdated(this, EventArgs.Empty); }
+            }
+        }
+        #endregion (ResetAnimation)
+
+        //-------------------------------------------------------------------------------
+        #region -NextFrame 次のフレームへ
+        //-------------------------------------------------------------------------------
+        //
+        private void NextFrame()
+        {
+            lock (_lockFrame) {
+                // 終わりか判別
+                if (!_animating || (!LoopInfinity && NowLoopCount == MaxLoopCount && NowFrameCount == MaxFrameCount)) {
+                    StopAnimation();
+                }
+
+                // フレームを進める
+                if (NowFrameCount == MaxFrameCount) {
+                    _nowFrameCount = 0;
+                    unchecked { _nowLoopCount++; }
+                }
+                else { _nowFrameCount++; }
+
+                _timer.Change(FrameDelays[_nowFrameCount] * 10, Timeout.Infinite);
+                _image.SelectActiveFrame(FrameDimension, _nowFrameCount);
+
+                if (FrameUpdated != null) { FrameUpdated(this, EventArgs.Empty); }
+            }
+        }
+        #endregion (NextFrame)
+
+        //-------------------------------------------------------------------------------
+        #region IDisposable.Dispose 破棄
+        //-------------------------------------------------------------------------------
+        /// <summary>
+        /// リソースを破棄します。イメージは破棄しません。
+        /// </summary>
+        public void Dispose()
+        {
+            lock (_lockFrame) { _timer.Dispose(); }
+        }
+        #endregion (IDisposable.Dispose)
+        //-------------------------------------------------------------------------------
+        #region +Dispose 破棄
+        //-------------------------------------------------------------------------------
+        /// <summary>
+        /// リソースを破棄します。
+        /// </summary>
+        /// <param name="disposeImage">画像を破棄する場合はtrue,しない場合はfalse</param>
+        public void Dispose(bool disposeImage)
+        {
+            lock (_lockFrame) {
+                if (disposeImage) { _image.Dispose(); }
+                Dispose();
+            }
+        }
+        #endregion (Dispose)
+    }
+}
